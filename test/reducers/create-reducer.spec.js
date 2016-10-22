dd489cc455b9fb6a7a9d1cb21bc97bcf825c6423
Stefan Rimaila <stefan@fusetools.com>
2016-10-22 14:42:22 +0200

    Add pending test

---
+++ test/reducers/create-reducer.spec.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import createReducer from '../../app/reducers/_create-reducer';
+
+describe('reducers', () => {
+  describe('createReducer', () => {
+    it('should create a usable reducer');
+  });
+});
